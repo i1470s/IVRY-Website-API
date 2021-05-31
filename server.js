8f748a2bfa0afb6bf4ed5b64d217ed90a402e1af
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-31 01:23:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 const app = express();
 
+Access-Control-Allow-Origin: *
+
 var corsOptions = {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
