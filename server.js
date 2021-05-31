d2928e9c685d3ba97a66aa338270dcdd7d282461
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-31 01:21:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://api-ivry.herokuapp.com/api/auth/signin"
-};
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+});
 
 app.use(cors(corsOptions));
 
